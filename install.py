59f50e0d2dd987675a9f76cebe7f2e6e6c7fe93c
spawner <spawner6@outlook.com>
2025-04-03 20:42:14 +0800

    Update install.py

--- install.py
+++ install.py
@@ -6,7 +6,7 @@ def install():
         launch.run_pip("install modelscope", "requirements for Wan Video Generator")
     
     if not launch.is_installed("diffsynth"):
-        launch.run_pip("install git+https://gitclone.com/github.com/modelscope/DiffSynth-Studio", 
+        launch.run_pip("install git+https://github.com/modelscope/DiffSynth-Studio", 
                       "requirements for Wan Video Generator - DiffSynth-Studio")
 
 if __name__ == "__main__":
